2c1f89ca74ef599d3e828aed3bfdd2d9473db6a8
yarin <sryybhndy@gmail.com>
2026-04-22 17:40:30 +0300

    feat(i18n): install next-intl, add config + navigation helpers

---
+++ lib/i18n/navigation.ts
@@ -0,0 +1,8 @@
+import { createSharedPathnamesNavigation } from "next-intl/navigation"
+
+export const locales = ["he", "en", "ru"] as const
+export type Locale = (typeof locales)[number]
+export const defaultLocale: Locale = "he"
+
+export const { Link, redirect, usePathname, useRouter } =
+  createSharedPathnamesNavigation({ locales })
